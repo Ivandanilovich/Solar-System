419f46eda7ec80824e0c90c66e8a52e0c21b26a7
Unknown <ivandanilovich99@gmail.com>
2019-06-11 16:13:20 +0700

    add logic

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -9,6 +9,7 @@ public class CanvasController : MonoBehaviour
     public GameObject canvas;
     public Text text1, text2, text3;
     public Text planetName;
+    public Dropdown dropdown;
 
     void Start()
     {
@@ -16,13 +17,43 @@ public class CanvasController : MonoBehaviour
         Hide();
     }
 
-    public void showInfo(int id = 3)
+    public void showInfo()
     {
+        var id = dropdown.GetComponent<Dropdown>().value;
         canvas.gameObject.SetActive(true);
-        planetName.text = Stuff.mercury["namerus"];
-        text1.text = Stuff.mercury["dist"];
-        text2.text = Stuff.mercury["weight"];
-        text3.text = Stuff.mercury["desc"];
+        Dictionary<string, string> v;
+        switch (id)
+        {
+            case 0:
+                v = Stuff.mercury;
+                break;
+            case 1:
+                v = Stuff.venus;
+                break;
+            case 2:
+                v = Stuff.earth;
+                break;
+            case 3:
+                v = Stuff.mars;
+                break;
+            case 4:
+                v = Stuff.jupiter;
+                break;
+            case 5:
+                v = Stuff.uranus;
+                break;
+            case 6:
+                v = Stuff.neptune;
+                break;
+            default:
+                v = Stuff.earth;
+                break;
+        }
+
+        planetName.text = v["namerus"];
+        text1.text = v["dist"];
+        text2.text = v["weight"];
+        text3.text = v["desc"];
     }
 
     public void Hide()
