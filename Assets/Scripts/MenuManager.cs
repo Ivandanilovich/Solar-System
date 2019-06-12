388b3e84ad22b41426bf3e4ce5c263254c4978a5
Unknown <ivandanilovich99@gmail.com>
2019-06-13 00:21:18 +0700

    add About scene

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -16,4 +16,9 @@ public class MenuManager : MonoBehaviour
     {
         SceneManager.LoadScene("Main");
     }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("First Scene");
+    }
 }
--- Assets/Scripts/MyPlanet.cs
+++ Assets/Scripts/MyPlanet.cs
@@ -1,7 +1,5 @@
 ﻿using Assets.Scripts;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MyPlanet : IPlanet
@@ -15,8 +13,7 @@ public class MyPlanet : IPlanet
 
     public Vector3 AxisRotate()
     {
-        
-        return new Vector3(0, (float)sigma * dayC,  0);
+        return new Vector3(0, (float)sigma * dayC, 0);
     }
 
     public Vector3 PRotate(float ti)
@@ -25,12 +22,9 @@ public class MyPlanet : IPlanet
             Math.Pow(obj.transform.position.x, 2) +
             Math.Pow(obj.transform.position.y, 2) +
             Math.Pow(obj.transform.position.z, 2)
-            ))) * 0.01f * 0.1f;
-
-
+            ))) * 0.001f;
 
         var r = p / (1 + e * Math.Cos(sigma));
-
         var y = 0;
         var x = r * Math.Cos(sigma);
         var z = r * Math.Sin(sigma);
@@ -43,6 +37,4 @@ public class MyPlanet : IPlanet
         p = rp * (1 + e);
         return new Vector3(rp, 0, 0);
     }
-
-
 }
